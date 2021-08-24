c4119da3cab09bde32e6576ed38e0e371be31fa6
dcq <1711708217@qq.com>
2021-08-24 12:48:41 +0800

    初始化项目

---
+++ rrrredis/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="stylus">
+body
+  background: var(--color-bg)
+  color: var(--color-text)
+
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color: var(--color-text)
+  margin-top 60px
+</style>
